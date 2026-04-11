79e9974ad66ee058789e5761717c80f79e3b0291
bilalsarwar2907 <bilalsarwar2907@gmail.com>
2026-04-11 13:19:13 +0200

    Add Member model with validation
       - Properties: Id, ClubName, Name, Count
       - Validation: Name >= 2 chars, Count 100-199
       - Override ToString()

---
+++ Club100API/Models/Member.cs
@@ -0,0 +1,40 @@
+﻿namespace Club100API.Models
+{
+    public class Member
+    {
+        public int Id { get; set; }
+        public string ClubName { get; set; }= string.Empty;
+
+        private string _name;
+
+        public string Name
+        {
+            get {return _name; } 
+            set { if(string.IsNullOrEmpty(value)|| value.Trim().Length < 2)
+
+                {
+                    throw new ArgumentOutOfRangeException("Name must be at least 2 characters"); 
+                }
+                _name = value; 
+            }
+        }
+        private int _count;
+
+        public int Count
+        {
+            get { return _count; }
+            set
+            {
+                if (value < 100 || value > 199)
+                {
+                    throw new ArgumentException("Count must be between 100 and 199.");
+                }
+                _count = value;
+            }
+        }
+        public override string ToString()
+        {
+            return $"Id: {Id}, ClubName: {ClubName}, Name: {Name}, Count: {Count}";
+        }
+    }
+}
