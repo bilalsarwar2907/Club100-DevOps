ed712a6824ff25d2f0b6ca2546491cf5d1e47d54
bilalsarwar2907 <bilalsarwar2907@gmail.com>
2026-04-11 21:12:22 +0200

    Add in-memory repository with seed data added Interfacr and Installed all relevent packages

---
+++ Club100API/Repositories/IMembersRepository.cs
@@ -0,0 +1,14 @@
+﻿using Club100API.Models;
+
+namespace Club100API.Repositories
+{
+    public interface IMembersRepository
+    {
+        Member Add(Member member);
+        Member? Delete(int id);
+        IEnumerable<Member> GetAll();
+        Member? GetById(int id);
+        IEnumerable<Member> GetMembersByCountAndName(int? minCount, int? maxCount, string? nameFilter);
+        Member? Update(Member member);
+    }
+}
